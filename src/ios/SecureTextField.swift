25596651b24e94a2a7e6808bd9f9b19781598860
ChaiwatCode <codechaiwat@gmail.com>
2024-12-24 15:38:23 +0700

    Update SecureTextField.swift

--- src/ios/SecureTextField.swift
+++ src/ios/SecureTextField.swift
@@ -1,33 +1,33 @@
-import Cordova
+import Foundation
+import UIKit
 
-@objc(SecureWindowPlugin) class SecureWindowPlugin: CDVPlugin {
-    @objc(makeSecureWindow:)
-    func makeSecureWindow(command: CDVInvokedUrlCommand) {
-        // Access the main window
-        guard let window = UIApplication.shared.windows.first else {
-            let error = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Unable to find window")
-            self.commandDelegate.send(error, callbackId: command.callbackId)
-            return
+@objc(SecureWindow)
+class SecureWindow: CDVPlugin {
+    @objc(makeSecure:)
+    func makeSecure(command: CDVInvokedUrlCommand) {
+        if let window = UIApplication.shared.windows.first {
+            window.makeSecure()
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Window secured")
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        } else {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Failed to secure window")
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
-        
+    }
+}
+
+extension UIWindow {
+    func makeSecure() {
         let field = UITextField()
         let view = UIView(frame: CGRect(x: 0, y: 0, width: field.frame.width, height: field.frame.height))
         field.isSecureTextEntry = true
-        window.addSubview(field)
-        
-        // Align the field to the center
+        self.addSubview(field)
         field.translatesAutoresizingMaskIntoConstraints = false
-        field.centerYAnchor.constraint(equalTo: window.centerYAnchor).isActive = true
-        field.centerXAnchor.constraint(equalTo: window.centerXAnchor).isActive = true
-        
-        // Make the text field's view the secure layer
-        field.layer.superlayer?.addSublayer(field.layer)
-        field.layer.sublayers?.last?.addSublayer(window.layer)
-        
+        field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.last?.addSublayer(self.layer)
         field.leftView = view
         field.leftViewMode = .always
-
-        let result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate.send(result, callbackId: command.callbackId)
     }
 }
